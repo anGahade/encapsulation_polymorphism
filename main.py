6f90fe491dbe06ce732bb4992616e6373a00353e
Gahade <angrygahade@gmail.com>
2023-08-20 12:13:43 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+"""
+Напишіть клас "Person", який має властивості name (ім'я) і age (вік).
+Зробіть ці властивості приватними, щоб вони не могли бути змінені напряму ззовні класу.
+Забезпечте методи для доступу до цих властивостей та встановлення їх значень.
+"""
+
+
+class Person:
+
+    def __int__(self, name, age):
+        self.__name = name
+        self.__age = age
+
+    def set_name(self, name):
+        self.__name = name
+        return name
+
+    def get_name(self):
+        return self.__name
+
+    def set_age(self, age):
+        self.__age = age
+        return age
+
+    def get_age(self):
+        return self.__age
+
+
+person = Person()
+person.set_name("John")
+person.set_age(25)
+print(person.get_name())
+print(person.get_age())
+
+        
