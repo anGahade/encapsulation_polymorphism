3b59fd7a4c269ba23dcf03ad6ea85ddc77e9a4dc
Gahade <angrygahade@gmail.com>
2023-08-20 14:14:24 +0200

    Add Parent class Shape with method display_color. Add Child class Rectangle with methods: width, height

--- main.py
+++ main.py
@@ -1,44 +1,42 @@
 """
-Розробіть клас "Car", який містить такі властивості:
-make (марка автомобіля), model (модель автомобіля), year (рік випуску автомобіля) і mileage (пробіг автомобіля).
-Забезпечте можливість доступу до цих властивостей через методи, а також зробіть властивості "make" і "model" приватними.
-Додайте метод "drive" до класу "Car", який збільшує пробіг автомобіля на задане значення.
-Перевірте, чи не перевищує пробіг заданий ліміт (наприклад, 300 000 км),
-і виведіть повідомлення про досягнення ліміту при спробі здійснити поїздку.
+Створіть базовий клас "Shape" (фігура), який має властивість color (колір)
+і метод display_color() для виведення коліру фігури.
+Створіть похідний клас "Rectangle" (прямокутник), який наслідує властивість color
+і додає властивості width (ширина) і height (висота).
+Забезпечте можливість встановлення значень ширини і висоти прямокутника та виведення їх значень.
 """
 
 
-class Car:
-    def __init__(self, make, model, year, mileage):
-        self.__make = make
-        self.__model = model
-        self.year = year
-        self.mileage = mileage
+class Shape:
+    def __init__(self, color):
+        self.color = color
 
-    def get_make(self):
-        return self.__make
-
-    def get_model(self):
-        return self.__model
-
-    def get_year(self):
-        return self.year
-
-    def get_mileage(self):
-        return self.mileage
-
-    def drive(self, miles_driven):
-        self.mileage = self.mileage + miles_driven
-        if self.mileage >= 250000:
-            print(f"Бобик сдох, далі не поїду, дзвони у сервіс, бо мій пробіг вже: {self.mileage} км")
-        else:
-            print(f"Загальний пробіг становить: {self.mileage} км")
+    def display_color(self):
+        return print(f"Color: {self.color}")
 
 
-car = Car("Toyota", "Camry", 2020, 5000)
-print(car.get_make()) # Виведе "Toyota"
-print(car.get_model()) # Виведе "Camry"
-print(car.get_year()) # Виведе 2020
-print(car.get_mileage()) # Виведе 5000
-car.drive(100) # Збільшення пробігу на 100
-car.drive(300000000) # Виведе повідомлення про досягнення ліміту
+class Rectangle(Shape):
+    def __init__(self, color, width, height):
+        super().__init__(color)
+        self.width = width
+        self.height = height
+
+    def display_color(self):
+        super().display_color()
+        # return print(f"Color: {self.color}")
+
+    def width(self):
+        return self.width()
+
+    def height(self):
+        return self.height()
+
+
+shape = Shape("Red")
+shape.display_color() # Виведе "Color: Red"
+rectangle = Rectangle("Blue", 10, 5)
+rectangle.display_color() # Виведе "Color: Blue"
+print(rectangle.width) # Виведе 10
+print(rectangle.height) # Виведе 5
+
+
